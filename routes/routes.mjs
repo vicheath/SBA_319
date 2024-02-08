6911ce7918abdfea7066b9af1a1d0b238c8c6325
vicheath <vicheath.a.s@gmail.com>
2024-02-08 01:49:33 -0500

    delete route

--- routes/routes.mjs
+++ routes/routes.mjs
@@ -5,35 +5,36 @@ import Comment from '../models/comments.mjs'
 
 const router = express.Router()
 
-// router.get("/:id", async (req, res) => {
-//   let foundGrade = await Comment.findById(req.params.id)
-//   res.status(200).json({
-//     data: foundGrade
-//   })
-// })
+
 
 router.get("/comments", async (req, res) => {
   try {
-      // Fetch customers from the 'customers' collection with a limit of 25
-      const customers = await db.collection('comments').find().limit(25).toArray();
-      
-      res.json(customers);
+      // Fetch comments from the 'comments' collection with a limit of 25
+      const comments = await db.collection('comments').find().limit(25).toArray();   
+      res.json(comments);
   } catch (error) {
       // Handle any errors
-      console.error('Error fetching customers:', error);
+      console.error('Error fetching comments:', error);
       res.status(500).send('Internal Server Error');
   }
 });
 
+router.get('/comments/names', async (req, res) => {
+  let foundComment = await Comment.findById().limit(50)
+  res.status(200).json({
+    foundComment: foundComment
+  })
+})
+
+
+
+
 //Get route for movies
 router.get("/movies", async (req, res) => {
   try {
-      // Fetch customers from the 'customers' collection with a limit of 25
-      const movies = await db.collection('movies').find().limit(25).toArray();
-      
+      const movies = await db.collection('movies').find().limit(25).toArray(); 
       res.json(movies);
   } catch (error) {
-      // Handle any errors
       console.error('Error fetching movies:', error);
       res.status(500).send('Internal Server Error');
   }
@@ -42,17 +43,30 @@ router.get("/movies", async (req, res) => {
 //Get route for users
 router.get("/users", async (req, res) => {
   try {
-      // Fetch customers from the 'customers' collection with a limit of 25
       const users = await db.collection('users').find().limit(25).toArray();
-      
       res.json(users);
   } catch (error) {
-    
       console.error('Error fetching users:', error);
       res.status(500).send('Internal Server Error');
   }
 });
 
+
+// GET route for fetching a single user by ID
+router.get("/users/:id", async (req, res) => {
+  try {
+      let collection = await db.collection("users");
+      let query = { _id: new ObjectId(req.params.id) };
+      let result = await collection.findOne(query);
+  
+      if (!result) res.send("Not found").status(404);
+      else res.send(result).status(200);
+  } catch (error) {
+      // Handle any errors
+      console.error('Error fetching users:', error);
+      res.status(500).send('Internal Server Error');
+  }
+});
 
 
 export default router;
